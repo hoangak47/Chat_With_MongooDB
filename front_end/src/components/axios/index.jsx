d5bcf4df4444703affc170e2198598d1fe941dac
hoangak47 <hoang41911@gmail.com>
2023-08-12 21:37:18 +0700

    update

--- front_end/src/components/axios/index.jsx
+++ front_end/src/components/axios/index.jsx
@@ -1,10 +1,17 @@
 import axios from 'axios';
 
 function axiosJWT() {
-    const axiosJWT = axios.create();
-
-    //cookie refresh token
-    console.log(document.cookie);
+    const axiosJWT = axios.create({
+        baseURL: 'http://localhost:5000/',
+        withCredentials: true,
+        credentials: 'include',
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+            'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token , Authorization',
+            'Content-Type': 'application/json',
+        },
+    });
 }
 
 export default axiosJWT;
