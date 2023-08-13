cdac3e051a8e160a2fae92a8326854d65df5b749
hoangak47 <hoang41911@gmail.com>
2023-08-13 22:03:24 +0700

    a

--- front_end/src/components/connnectSocket/index.jsx
+++ front_end/src/components/connnectSocket/index.jsx
@@ -3,6 +3,7 @@ import { io } from 'socket.io-client';
 const socket = io('https://chat-with-mongoo-db-l2fr.vercel.app', {
     withCredentials: true,
     path: '/socket.io',
+    transports: ['websocket'],
 });
 
 export default socket;
